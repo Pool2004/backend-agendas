09e45a64a860b8e195bf2f936aeeeef51e2b7619
= <jeandeveloper04@gmail.com>
2026-06-22 10:10:27 -0500

    fix: test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routes import router
+from routes import router
 
 # Inicialización de la aplicación FastAPI con metadatos descriptivos
 app = FastAPI(
